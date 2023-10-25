ab978e859119e679904f227fa83396c4826ad171
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2023-10-25 09:44:54 +0530

    25-10-23

--- exp6/query.sql
+++ exp6/query.sql
@@ -1,72 +1,86 @@
+CREATE TABLE items(
+	itemid CHAR(6) PRIMARY KEY,
+	itemname VARCHAR(50),
+	category VARCHAR(20),
+	price INT,
+	instock INT);
 
-ALTER TABLE customer RENAME COLUMN city TO state;
+INSERT INTO items VALUES('000001','Samsung GalaxyS4','Electronics',50000,10);
+INSERT INTO items VALUES('000002','iPhone 12','Electronics',70000,5);
+INSERT INTO items VALUES('000003','HP Laptop','Electronics',60000,8);
+INSERT INTO items VALUES('000004','Canon DSLR','Electronics',80000,6);
 
-alter table customer drop (POSTAL,  COUNTRY, CONTACTNAME);
+SELECT * FROM items;
 
-create table customers ( id int primary key, name varchar(20), address varchar(40), state varchar(20));
+CREATE TABLE customers(
+	custid CHAR(6) PRIMARY KEY,
+	custname VARCHAR(20),
+	address VARCHAR(20),
+	state VARCHAR(10));
+
+INSERT INTO customers VALUES('A1B2C3','Sara','Main St','Mumbai');
+INSERT INTO customers VALUES('D4E5F6','Elvin','Maple Ave','Mumbai');
+INSERT INTO customers VALUES('G7H8I9','Liam','Oak St','Delhi');
+INSERT INTO customers VALUES('J0K1L2','Emma','Pine Ave','Delhi');
+
+SELECT * FROM customers;
+
+CREATE TABLE orders(
+	orderid CHAR(6) PRIMARY KEY,
+	itemid CHAR(6),
+	custid CHAR(6),
+	quantity INT,
+	orderdate DATE,
+	FOREIGN KEY (itemid) REFERENCES items(itemid),
+	FOREIGN KEY (custid) REFERENCES customers(custid));
+
+INSERT INTO orders VALUES('100001','000001','A1B2C3',2,'01-JAN-2023');
+INSERT INTO orders VALUES('100002','000002','D4E5F6',1,'21-MAR-2023');
+INSERT INTO orders VALUES('100003','000003','G7H8I9',1,'13-MAR-2023');
+INSERT INTO orders VALUES('100004','000004','J0K1L2',1,'27-APR-2023');
+INSERT INTO orders VALUES('100005','000002','A1B2C3',1,'08-APR-2023');
+INSERT INTO orders VALUES('100006','000001','D4E5F6',2,'29-APR-2023');
+
+SELECT * FROM orders;
+
+CREATE TABLE delivery(
+	deliveryid CHAR(6) PRIMARY KEY,
+	custid CHAR(6),
+	orderid CHAR(6),
+	FOREIGN KEY (custid) REFERENCES customers(custid),
+	FOREIGN KEY (orderid) REFERENCES orders(orderid));
+
+INSERT INTO delivery VALUES('100100','A1B2C3','100001');
+INSERT INTO delivery VALUES('100200','D4E5F6','100002');
+INSERT INTO delivery VALUES('100300','G7H8I9','100003');
+INSERT INTO delivery VALUES('100400','J0K1L2','100004');
+
+SELECT * FROM delivery;
 
 
-create table items (id int PRIMARY KEY, name varchar(50), category varchar(20), price int, instock int);
+
+SELECT * FROM customers WHERE custid IN(SELECT custid FROM orders);
 
 
-create table orders (id int primary key, iid int, cid int, FOREIGN KEY (iid) REFERENCES items(id), FOREIGN KEY (cid) REFERENCES customers(id), quantity int, dates varchar(20));
+SELECT * FROM customers WHERE custid IN(SELECT custid FROM delivery);
 
 
-create table delivery ( id int primary key, cid int, oid int, FOREIGN KEY (cid) REFERENCES customers(id), FOREIGN KEY (oid) REFERENCES order(id) );
+SELECT orderdate FROM orders WHERE custid IN(SELECT custid FROM customers WHERE SUBSTR(custname,1,1) = 'S');
 
 
+SELECT itemname, price FROM items WHERE itemid IN(
+	SELECT o.itemid FROM orders o
+		LEFT JOIN customers c ON o.custid = c.custid
+		WHERE c.custname = 'Elvin');
 
 
-INSERT INTO customer values (1, 'Amit Sharma', '123 Gandhi Road', 'Mumbai',);
-INSERT INTO customer values   (2, 'Priya Patel', '456 Taj Mahal Street', 'Delhi',);
-INSERT INTO customer values   (3, 'Elvin', '789 Lotus Lane', 'Bangalore',);
-INSERT INTO customer values   (4, 'Anita Kapoor',  '234 Spice Market', 'Chennai',);
-INSERT INTO customer values   (5, 'Vikram Verma', '567 Tandoori Avenue', 'Hyderabad',);
-INSERT INTO customer values   (6, 'Meera Nair', '890 Bollywood Road', 'Kolkata',);
-INSERT INTO customer values   (7, 'Sanjay Reddy', '101 Coconut Lane', 'Kochi',);
-INSERT INTO customer values (8, 'Sunny', 'Wei',eijing Road', 'Beijing',);
-INSERT INTO customer values   (9, 'Xiaoming Chen', '456 Great Wall Street', 'Shanghai',);
-INSERT INTO customer values   (10, 'Sasi', '789 Forbidden City Lane', 'Xian',);
+SELECT * FROM customers WHERE custid IN(
+	SELECT custid FROM orders WHERE orderid NOT IN(
+		SELECT orderid FROM delivery)
+		AND orderdate > '31-JAN-2013');
 
 
+SELECT itemid FROM orders UNION(
+	SELECT itemid FROM orders WHERE orderid NOT IN(
+		SELECT orderid FROM delivery));
 
-
-INSERT INTO items VALUES (1, 'Apple', 'Fruit', 6, 100);
-INSERT INTO items VALUES (2, 'Banana', 'Fruit', 25, 50);
-INSERT INTO items VALUES (3, 'Orange', 'Fruit', 75, 75);
-INSERT INTO items VALUES (4, 'Milk', 'Dairy', 25, 20);
-INSERT INTO items VALUES (5, 'Cheese', 'Dairy', 30, 10);
-INSERT INTO items VALUES (6, 'Samsung Galaxy S4', 'Tech', 5000, 10);
-
-
-INSERT INTO orders VALUES (1, 1, 1, 10, '2023-10-11');
-INSERT INTO orders VALUES (2, 2, 2, 5, '2023-10-11');
-INSERT INTO orders VALUES (3, 3, 3, 7, '2023-10-11');
-INSERT INTO orders VALUES (4, 4, 4, 2, '2023-10-11');
-INSERT INTO orders VALUES (5, 5, 5, 1, '2023-10-11');
-INSERT INTO orders VALUES (6, 6, 6, 8, '2023-10-11');
-INSERT INTO orders VALUES (7, 7, 7, 4, '2023-10-11');
-INSERT INTO orders VALUES (8, 8, 8, 6, '2023-10-11');
-INSERT INTO orders VALUES (9, 9, 9, 3, '2023-10-11');
-INSERT INTO orders VALUES (10, 10, 10, 12,'2023-10-11');
-
-INSERT INTO delivery (id, cid, oid) VALUES (1, 1, 1);
-INSERT INTO delivery (id, cid, oid) VALUES (2, 2, 2);
-INSERT INTO delivery (id, cid, oid) VALUES (3, 3, 3);
-INSERT INTO delivery (id, cid, oid) VALUES (4, 4, 4);
-INSERT INTO delivery (id, cid, oid) VALUES (5, 5, 5);
-INSERT INTO delivery (id, cid, oid) VALUES (6, 6, 6);
-INSERT INTO delivery (id, cid, oid) VALUES (7, 7, 7);
-INSERT INTO delivery (id, cid, oid) VALUES (8, 8, 8);
-INSERT INTO delivery (id, cid, oid) VALUES (9, 9, 9);
-INSERT INTO delivery (id, cid, oid) VALUES (10, 10, 10);
-INSERT INTO delivery (id, cid, oid) VALUES (11, 11, 11);
-INSERT INTO delivery (id, cid, oid) VALUES (12, 12, 12);
-INSERT INTO delivery (id, cid, oid) VALUES (13, 13, 13);
-INSERT INTO delivery (id, cid, oid) VALUES (14, 14 ,14);
-INSERT INTO delivery (id,cid ,oid )VALUES(15 ,15 ,15 );
-INSERT INTO delivery(id,cid ,oid )VALUES(16 ,16 ,16 );
-INSERT INTO delivery(id,cid ,oid )VALUES(17 ,17 ,17 );
-INSERT INTO delivery(id,cid ,oid )VALUES(18 ,18 ,18 );
-INSERT INTO delivery(id,cid ,oid )VALUES(19 ,19 ,19 );
-INSERT INTO delivery(id,cid ,oid )VALUES(20 ,20 ,20 );
